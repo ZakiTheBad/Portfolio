3fff10b9d9a40bbe685be92bfd646d0e46f2764d
ZakiTheBad <77641333+ZakiTheBad@users.noreply.github.com>
2026-09-18 16:32:12 +0200

    Simplification du footer : copyright uniquement

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,67 +1,26 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import {
-  AiFillGithub,
-  AiOutlineTwitter,
-  AiFillInstagram,
-} from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
-  let date = new Date();
-  let year = date.getFullYear();
   return (
-    <Container fluid className="footer">
+    <Container
+      fluid
+      className="footer"
+      style={{
+        width: "100%",
+        marginTop: "40px",
+        padding: "18px 0",
+      }}
+    >
       <Row>
-        <Col md="4" className="footer-copywright">
-          <h3>Designed and Developed by Soumyajit Behera</h3>
-        </Col>
-        <Col md="4" className="footer-copywright">
-          <h3>Copyright © {year} SB</h3>
-        </Col>
-        <Col md="4" className="footer-body">
-          <ul className="footer-icons">
-            <li className="social-icons">
-              <a
-                href="https://github.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillGithub />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://twitter.com/Soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiOutlineTwitter />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <FaLinkedinIn />
-              </a>
-            </li>
-            <li className="social-icons">
-              <a
-                href="https://www.instagram.com/soumyajit4419"
-                style={{ color: "white" }}
-                target="_blank" 
-                rel="noopener noreferrer"
-              >
-                <AiFillInstagram />
-              </a>
-            </li>
-          </ul>
+        <Col
+          md="12"
+          className="footer-copywright"
+          style={{ textAlign: "center" }}
+        >
+          <h3 style={{ fontSize: "1rem", margin: 0 }}>
+            Copyright © 2026 Jawad MERDJI
+          </h3>
         </Col>
       </Row>
     </Container>
